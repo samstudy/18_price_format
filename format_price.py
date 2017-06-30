709e9fd7fc32098f991a523a0636e81c1952b95c
sam156 <bskordo@gmail.com>
2017-06-30 07:56:35 -0400

    initial version

--- format_price.py
+++ format_price.py
@@ -1,5 +1,17 @@
-def format_price(price):
-    pass
+import re
 
-if __name__ == '__main__':
-    pass
+
+def format_price(price):
+    if type(price) is int:
+        updated_price = "{:,}".format(price)
+        return (updated_price.replace(',',' '))
+    if type(price) is float:
+        updated_price = ('%f' % price).rstrip('0').rstrip('.')
+        if re.findall(r'\.', updated_price):
+            formated_price = "{:,}".format(float(updated_price))
+            return (formated_price.replace(',',' '))
+        else:
+            formated_price = "{:,}".format(int(updated_price))
+            return(formated_price.replace(',',' '))
+    else:
+        raise ValueError('Incorrect value')
